48df4231bb53df98bcc4ff9f5dee955da6fc0ee0
Razi Alawamleh <razi.alawamleh@gmail.com>
2023-12-31 14:54:45 -0500

    Day 4

---
+++ Day 4/day4.py
@@ -0,0 +1,116 @@
+print("NSFW")
+print("The following is your chance to help a famous American Rapper")
+print("Your job will be to answer a few questions. Easy right?")
+print("If you figure out the song and the rapper, play the song and try your new lyrics.")
+
+
+
+print("""Look, my bitches all bad, my *bleep* all real
+I ride his dick, in some big tall heels
+Big fat checks, big large bills
+Front, I'll flip like ten cartwheels
+Cold ass bitch, I give Ross chills
+Ten different looks and my looks all kill
+I kiss him in the mouth, I feel all grills
+He eat in the car, that's meals on wheels (woo)\033[32m
+
+I was born to flex (yes)
+Diamonds on my neck
+I like boardin' jets, I like mornin' sex (woo)\033[32m
+But nothing in this world
+That I like more than checks \033[32m(money)\033[32m
+All I really wanna see is the \033[32m(money)\033[32m
+I don't really need the D, I need the \033[32m(money)\033[32m
+All a bad bitch need is the \033[32m(money flow)\033[32m
+
+I got bands in the coupe (coupe)\033[32m
+Bustin' out the roof
+I got bands in the coupe (coupe)\033[32m
+Touch me, I'll shoot (bow)\033[32m
+
+Shake a little ass \033[32m(money)
+Get a little bag and take it to the store \033[32m(money)\033[32m
+Get a little cash \033[32m(money)\033[32m
+You shake it real fast, you get a little more \033[32m(money)\033[32m
+I got bands in the coupe (coupe)\033[32m
+Bustin' out the roof
+I got bands in the coupe (brrr)\033[32m
+Bustin' out the roof (Cardi)\033[32m
+
+I gotta fly, I need a jet, shit
+I need room for my legs
+I got a baby, I need some money, yeah
+I need cheese for my egg
+All y'all bitches in trouble
+Bring brass knuckles to the scuffle
+I heard that Cardi went pop
+Yeah, I did go pop (pop)\033[32m
+That's me bustin' they bubble
+
+I'm Dasani with the drip
+Baby mommy with the clip
+Walk out Follie's with a bitch
+Bring a thottie to the whip
+If she fine or she thick, goddamn
+Walkin' past the mirror, ooh
+Damn, I'm fine (fine)\033[32m
+Let a bitch try me, boom (boom)\033[32m
+Hammer time, uh
+
+I was born to flex (yes)
+Diamonds on my neck
+I like boardin' jets, I like mornin' sex (woo)\033[32m
+But nothing in this world
+That I like more than checks \033[32m(money)\033[32m
+All I really wanna see is the \033[32m(money)\033[32m
+I don't really need the D, I need the \033[32m(money)\033[32m
+All a bad bitch need is the \033[32m(money flow)\033[32m
+
+I got bands in the coupe (coupe)\033[32m
+Bustin' out the roof
+I got bands in the coupe (coupe)\033[32m
+Touch me, I'll shoot (bow)\033[32m
+
+Shake a little ass (money)\033[32m
+Get a little bag and take it to the store (money)\033[32m
+Get a little cash (money)\033[32m
+You shake it real fast, you get a little more (money)\033[32m
+I got bands in the coupe (coupe)\033[32m
+Bustin' out the roof
+I got bands in the coupe (brrr)\033[32m
+Touch me, I'll shoot (bow)\033[32m
+
+Bitch, I will pop on your pops (your pops)\033[32m
+Bitch, I will pop on whoever (brrr)\033[32m
+You know who popped the most shit? (Who?)\033[32m
+The people whose shit not together (okay)\033[32m
+You'da bet Cardi a freak (freak)\033[32m
+All my pajamas is leather (uh)\033[32m
+Bitch, I will black on your ass, yeah (woo, woo)\033[32m
+Wakanda forever
+
+Sweet like a honey bun, spit like a Tommy gun (brrr)\033[32m
+Rollie a one of one, come get your mommy some
+Cardi at the tip-top, bitch
+Kiss the ring and kick rocks, sis (uh)\033[32m
+Jump it down, back it up (ooh, ayy)\033[32m
+Make that nigga put down 2K
+I like my niggas dark like D'USSÉ
+He gonna eat this ass like soufflé
+
+I was born to flex, diamonds on my neck
+I like boardin' jets, I like mornin' sex
+But nothing in this world
+That I like more than Kulture (Kulture, Kulture, Kulture)\033[32m
+All I really wanna see is the \033[32m(money)\033[32m
+I don't really need the D, I need the \033[32m(money)\033[32m
+All a bad bitch need is the K.K.C (woo)\033[32m
+
+\033[32m(Money)\033[32m
+\033[32m(Money)\033[32m
+\033[32m(Money)\033[32m
+\033[32m(Money)\033[32m
+\033[32m(Money)\033[32m
+\033[32m(Money)\033[32m
+\033[32m(Money)\033[32m
+\033[32m(Money)""")
