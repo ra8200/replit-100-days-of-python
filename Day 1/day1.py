8d5e97817252d7247386a7bb30aa694ece7afff3
Razi Alawamleh <razi.alawamleh@gmail.com>
2023-12-31 14:40:18 -0500

    100 Days of Python: Day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,7 @@
+print("""Razi A
+Today is November 13th, 2023
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+I am feeling 🙂
+You can follow my progress at replit.com/@razialawamlweh""")
