ce5d9372fb3e84cd3ae8cebd11db199a67193e0b
Razi Alawamleh <razi.alawamleh@gmail.com>
2023-12-31 15:07:06 -0500

    Day 5

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,9 +1,9 @@
-print("I got a few quesitons for you about stupid shit.")
+print("I got a few questions for you about stupid shit.")
 print()
 print("Just Answer the questions so the code is right")
 print()
 myName = input("Whats your name?: ")
-print("What a dewby name. Sorry I shouldn't have said that. anyway next qustion")
+print("What a dewby name. Sorry I shouldn't have said that. anyway next question")
 print()
 myPower = input("If you could have one of the 3 powers what would it be? Flight, Speed, or Strength.")
 print()
@@ -12,6 +12,6 @@ if myPower == "Flight" or "flight":
 elif myPower == "Speed" or "speed":
   print("Why? Because you want to fly...")
 elif myPower == "Strength" or "strength":
-  print("OOO big strong man, or woman, coming")
+  print("OOO big strong muscle mommy or daddy or whatever")
 else: 
   print("wrong answer. fail!")
